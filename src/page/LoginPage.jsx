f83762affdf4cc15a35e9c01f60901b4953242e5
pornrapat309 <pornrapat.kaew@gmail.com>
2023-11-02 15:28:18 +0700

    test

--- src/page/LoginPage.jsx
+++ src/page/LoginPage.jsx
@@ -8,7 +8,7 @@ export default function LoginPage() {
       <div className="h-1/2 bg-cover bg-center relative bg-[url('/src/assets/bg1.jpg')]"></div>
       <Link to="/register">
         <button className="absolute top-5 right-6 py-2 px-4 border border-black text-xs font-semibold z-10">
-          SIGN UP
+          SIGNUP
         </button>
       </Link>
       <div className="flex items-center justify-center absolute inset-0">
