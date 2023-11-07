bedfa7c1f15aeb116306b53455bc716efbf51a3b
Champ <droped17@gmail.com>
2023-11-07 13:50:14 +0700

    update EditResInfo (not fin: refactor)

--- src/components/EditResInfo.jsx
+++ src/components/EditResInfo.jsx
@@ -7,10 +7,14 @@ import DropdownDistrict from "../features/auth/DropdownDistrict";
 import profileImage from "../assets/logo.png";
 import axios from "../config/axios";
 import useRes from "../Hooks/use-res";
+import { allDistricts } from "../data/dataRes";
 
 export default function EditResInfo({ handleOnSubmit }) {
   const { business } = useRes();
   // console.log(business);
+
+
+  console.log(allDistricts);
 
   const fileEl = useRef(null);
   const [input, setInput] = useState({
@@ -29,60 +33,58 @@ export default function EditResInfo({ handleOnSubmit }) {
     businessTime: business,
   });
 
-  const districtIndex = [
-    "Bang Bon",
-    "Bang Kapi",
-    "Bang Khae",
-    "Bang Khen",
-    "Bang Kho Laem",
-    "Bang Khun Thian",
-    "Bang Na",
-    "Bang Phlat",
-    "Bang Rak",
-    "Bang Sue",
-    "Bangkok Noi",
-    "Bangkok Yai",
-    "Bueng Kum",
-    "Chatuchak",
-    "Chom Thong",
-    "Din Daeng",
-    "Don Mueang",
-    "Dusit",
-    "Huai Khwang",
-    "Khan Na Yao",
-    "Khlong Sam Wa",
-    "Khlong San",
-    "Khlong Toei",
-    "Lak Si",
-    "Lat Krabang",
-    "Lat Phrao",
-    "Min Buri",
-    "Nong Chok",
-    "Nong Khaem",
-    "Pathum Wan",
-    "Phasi Charoen",
-    "Phaya Thai",
-    "Phara Khanong",
-    "Phra Nakhon",
-    "Pom Prap Sattru Phai",
-    "Prawet",
-    "Rat Burana",
-    "Ratchathewi",
-    "Sai Mai",
-    "Samphanthawong",
-    "Saphan Sung",
-    "Sathon",
-    "Suan Luang",
-    "Taling Chan",
-    "Thawi Watthana",
-    "Thon Buri",
-    "Thung Khru",
-    "Wang Thonglang",
-    "Watthana",
-    "Yan Nawa",
-  ];
-
-  const [disIndex, setDisIndex] = useState([]);
+  // const districtIndex = [
+  //   "Bang Bon",
+  //   "Bang Kapi",
+  //   "Bang Khae",
+  //   "Bang Khen",
+  //   "Bang Kho Laem",
+  //   "Bang Khun Thian",
+  //   "Bang Na",
+  //   "Bang Phlat",
+  //   "Bang Rak",
+  //   "Bang Sue",
+  //   "Bangkok Noi",
+  //   "Bangkok Yai",
+  //   "Bueng Kum",
+  //   "Chatuchak",
+  //   "Chom Thong",
+  //   "Din Daeng",
+  //   "Don Mueang",
+  //   "Dusit",
+  //   "Huai Khwang",
+  //   "Khan Na Yao",
+  //   "Khlong Sam Wa",
+  //   "Khlong San",
+  //   "Khlong Toei",
+  //   "Lak Si",
+  //   "Lat Krabang",
+  //   "Lat Phrao",
+  //   "Min Buri",
+  //   "Nong Chok",
+  //   "Nong Khaem",
+  //   "Pathum Wan",
+  //   "Phasi Charoen",
+  //   "Phaya Thai",
+  //   "Phara Khanong",
+  //   "Phra Nakhon",
+  //   "Pom Prap Sattru Phai",
+  //   "Prawet",
+  //   "Rat Burana",
+  //   "Ratchathewi",
+  //   "Sai Mai",
+  //   "Samphanthawong",
+  //   "Saphan Sung",
+  //   "Sathon",
+  //   "Suan Luang",
+  //   "Taling Chan",
+  //   "Thawi Watthana",
+  //   "Thon Buri",
+  //   "Thung Khru",
+  //   "Wang Thonglang",
+  //   "Watthana",
+  //   "Yan Nawa",
+  // ];
 
   const [file, setFile] = useState(null);
   const [error, setError] = useState({});
@@ -238,17 +240,18 @@ export default function EditResInfo({ handleOnSubmit }) {
             name="districtIndex"
             id="districtIndex"
             className="w-full bg-transparent text-blue-gray-700 transition-all outline outline-0 border text-sm px-3 py-3 rounded-md border-blue-gray-200 focus:border-gray-900 focus:border-2"
-            onChange={(e) =>
+            onChange={(e) =>{
               setInput({ ...input, [e.target.name]: e.target.value })
+            }
             }
           >
             <option value="selected">DISTRICT</option>
-            {districtIndex.map((item, index) => {
+            {allDistricts.map((item, index) => {
               return <option
                 name="districtIndex"
                 key={index}
                 id={item.id}
-                value={item.id}
+                value={index + 1}
               >
                 {item}
               </option>;
