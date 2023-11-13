fe07df6c7e2955d55936a694c29aca6ce46f51ce
Thanatip-iceJ <p.thanatip1@gmail.com>
2023-11-13 10:54:45 +0700

    updated env path

--- src/main.jsx
+++ src/main.jsx
@@ -3,22 +3,22 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import AuthContextProvider from "./contexts/AuthContext.jsx";
-import { clientId } from "./config/env.js";
+import { clientId } from "../env.js";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import ResContextProvider from "./contexts/ResContext.jsx";
 import BookingContextProvider from "./contexts/BookingContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-	// <React.StrictMode>
-	<GoogleOAuthProvider clientId={clientId}>
-		<AuthContextProvider>
-			<ResContextProvider>
-				<BookingContextProvider>
-					<App />
-				</BookingContextProvider>
-			</ResContextProvider>
-		</AuthContextProvider>
-	</GoogleOAuthProvider>
+  // <React.StrictMode>
+  <GoogleOAuthProvider clientId={clientId}>
+    <AuthContextProvider>
+      <ResContextProvider>
+        <BookingContextProvider>
+          <App />
+        </BookingContextProvider>
+      </ResContextProvider>
+    </AuthContextProvider>
+  </GoogleOAuthProvider>
 
-	// </React.StrictMode>,
+  // </React.StrictMode>,
 );
