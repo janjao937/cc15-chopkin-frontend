4a28d82ec7f49e89fea883c6c3c88b082c4c069f
auttpon <auttaponcoe24@gmail.com>
2023-11-16 03:11:33 +0700

    merge

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,14 @@ import useAuth from "./Hooks/use-auth";
 import Loading from "./components/Loading";
 
 export default function App() {
-  const { initialLoading } = useAuth();
-  if (initialLoading) {
-    return <Loading />;
-  }
-  return (
-    <>
-      <Route />
-      <ToastContainer position="top-center" />
-    </>
-  );
+	const { initialLoading } = useAuth();
+	if (initialLoading) {
+		return <Loading />;
+	}
+	return (
+		<>
+			<Route />
+			<ToastContainer position="top-center" />
+		</>
+	);
 }
