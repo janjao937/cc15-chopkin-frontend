631ae6c77719414ba13c6cd109084f1fbd2fc9fa
Thanatip-iceJ <p.thanatip1@gmail.com>
2023-11-15 18:47:31 +0700

    fixed footer and header

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -7,12 +7,12 @@ import LiveChat from "./LiveChat";
 export default function Layout() {
   return (
     <div className="w-[100vw] min-h-[100vh]">
-      <div className="max-w-[1400px] mx-auto">
-        <Header />
+      <Header />
+      <div id="CONTAINER" className="max-w-[1400px] mx-auto">
         <LiveChat />
         <Outlet />
-        <Footer />
       </div>
+      <Footer />
     </div>
   );
 }
