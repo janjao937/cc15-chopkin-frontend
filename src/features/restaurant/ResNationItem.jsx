1478aad97bc1f81aaf16453e769180c1c21f4cab
pornrapat309 <pornrapat.kaew@gmail.com>
2023-11-15 23:44:18 +0700

    cuisine page render only approved  restaurant

--- src/features/restaurant/ResNationItem.jsx
+++ src/features/restaurant/ResNationItem.jsx
@@ -5,50 +5,44 @@ import { categoryIndex } from "../../data/dataRes";
 import { AiFillStar } from "react-icons/ai";
 
 export default function ResNationItem({
-	objData,
-	profileImg,
-	name,
-	catIndex,
-	price,
+  objData,
+  profileImg,
+  name,
+  catIndex,
+  price,
+  status,
 }) {
-	const category = categoryIndex.filter((item) => item.id === catIndex);
-	// console.log("category", category[0].title);
+  const category = categoryIndex.filter((item) => item.id === catIndex);
+  // console.log("category", category[0].title);
 
-	// console.log(objData);
+  // console.log(objData);
 
-	return (
-		<>
-			<Link to={`/restaurant/${objData.id}`}>
-				<div className="w-full h-[200px]">
-					<img
-						src={profileImg || blank}
-						alt={name}
-						className="w-full h-full object-fill rounded-t-3xl"
-					/>
-				</div>
-				<div className="p-4">
-					<div className="font-semibold text-lg">{name}</div>
-					<div className="font-light text-md ">
-						{category[0].title}
-					</div>
-					<div className="flex items-center justify-between">
-						<div className="flex items-center gap-1">
-							<div>
-								<AiFillStar
-									size={30}
-									className="text-yellow-400"
-								/>
-							</div>
-							<div className="text-primary font-semibold">
-								3.0
-							</div>
-						</div>
-						<div className="text-primary font-semibold">
-							{price}
-						</div>
-					</div>
-				</div>
-			</Link>
-		</>
-	);
+  return (
+    <>
+      {status === 1 && (
+        <Link to={`/restaurant/${objData.id}`}>
+          <div className="w-full h-[200px]">
+            <img
+              src={profileImg || blank}
+              alt={name}
+              className="w-full h-full object-fill rounded-t-3xl"
+            />
+          </div>
+          <div className="p-4">
+            <div className="font-semibold text-lg">{name}</div>
+            <div className="font-light text-md ">{category[0].title}</div>
+            <div className="flex items-center justify-between">
+              <div className="flex items-center gap-1">
+                <div>
+                  <AiFillStar size={30} className="text-yellow-400" />
+                </div>
+                <div className="text-primary font-semibold">3.0</div>
+              </div>
+              <div className="text-primary font-semibold">{price}</div>
+            </div>
+          </div>
+        </Link>
+      )}
+    </>
+  );
 }
--- src/features/restaurant/ResNationList.jsx
+++ src/features/restaurant/ResNationList.jsx
@@ -2,17 +2,18 @@ import React from "react";
 import ResNationItem from "./ResNationItem";
 
 export default function ResNationList({ data }) {
-	// console.log("nationList =>", data);
+  // console.log("nationList =>", data);
 
-	return (
-		<div className="border border-gray-200 rounded-3xl max-w-[350px] cursor-pointer hover:shadow-lg hover:transition">
-			<ResNationItem
-				objData={data}
-				profileImg={data.profileImg}
-				name={data.restaurantName}
-				catIndex={data.categoryIndex}
-				price={data.price}
-			/>
-		</div>
-	);
+  return (
+    <div className="border border-gray-200 rounded-3xl max-w-[350px] cursor-pointer hover:shadow-lg hover:transition">
+      <ResNationItem
+        objData={data}
+        profileImg={data.profileImg}
+        name={data.restaurantName}
+        catIndex={data.categoryIndex}
+        price={data.price}
+        status={data.status}
+      />
+    </div>
+  );
 }
