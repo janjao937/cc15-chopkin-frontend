2c468f71ede1b0808af7faee00552339b17ddfcf
Thanatip-iceJ <p.thanatip1@gmail.com>
2023-11-15 18:32:38 +0700

    added container for layout

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -5,12 +5,14 @@ import Footer from "./Footer";
 import LiveChat from "./LiveChat";
 
 export default function Layout() {
-	return (
-		<>
-			<Header />
-			<LiveChat/>
-			<Outlet />
-			<Footer />
-		</>
-	);
+  return (
+    <div className="w-[100vw] min-h-[100vh]">
+      <div className="max-w-[1400px] mx-auto">
+        <Header />
+        <LiveChat />
+        <Outlet />
+        <Footer />
+      </div>
+    </div>
+  );
 }
