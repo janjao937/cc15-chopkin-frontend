a55f9808d87c587e95396691f3aa49033fb7c244
Champ <droped17@gmail.com>
2023-11-16 21:17:15 +0700

    update animation

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,31 +1,35 @@
 import React from "react";
+import LoadingAnimation from "../icon/LoadingAnimation.json";
+import Lottie from "lottie-react";
 
-function LoaderIcon({ className = null }) {
-	return (
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			viewBox="0 0 20 20"
-			height="2rem"
-			width="2rem"
-			fill="#000"
-			className={className}
-		>
-			<g>
-				<path d="M10,1V3a7,7,0,1,1-7,7H1a9,9,0,1,0,9-9Z" />
-			</g>
-		</svg>
-	);
-}
+// function LoaderIcon({ className = null }) {
+// 	return (
+// 		<svg
+// 			xmlns="http://www.w3.org/2000/svg"
+// 			viewBox="0 0 20 20"
+// 			height="2rem"
+// 			width="2rem"
+// 			fill="#000"
+// 			className={className}
+// 		>
+// 			<g>
+// 				<path d="M10,1V3a7,7,0,1,1-7,7H1a9,9,0,1,0,9-9Z" />
+// 			</g>
+// 		</svg>
+// 	);
+// }
 
 export default function Loading() {
-	return (
-		<>
-			<div className="fixed inset-0 bg-black opacity-30 z-40"></div>
-			<div className="fixed inset-0 z-50">
-				<div className="flex items-center justify-center min-h-full">
-					<LoaderIcon className="fill-blue-600 animate-spin" />
-				</div>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="fixed inset-0 bg-black opacity-30 z-40"></div>
+      <div className="fixed inset-0 z-50">
+        <div className="flex items-center justify-center min-h-full">
+          <div className="">
+            <Lottie animationData={LoadingAnimation}></Lottie>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
--- src/page/LoginPage.jsx
+++ src/page/LoginPage.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import LoginForm from "../features/auth/LoginForm";
 import { Link } from "react-router-dom";
+import LoginAnimation from "../icon/LoginAnimation.json";
+import Lottie from "lottie-react";
 
 export default function LoginPage() {
   return (
-    <div className="h-screen flex flex-col relative">
-      <div className="h-1/2 bg-cover bg-center relative bg-[url('/src/assets/bg1.jpg')]"></div>
+    <div className="h-screen flex relative">
+      {/* <div className="h-1/2 bg-cover bg-center relative bg-[url('/src/assets/bg1.jpg')]"></div> */}
+    
+        <Lottie animationData={LoginAnimation}></Lottie>
+   
       <Link to="/register">
-        <button className="absolute top-5 right-6 py-2 px-4 border border-black text-xs font-semibold z-10">
+        <button className="absolute top-5 right-6 py-2 px-4 border text-xs font-semibold z-10">
           SIGN UP
         </button>
       </Link>
-      <div className="flex items-center justify-center absolute inset-0">
+      <div className="flex items-center justify-center">
         <LoginForm />
       </div>
     </div>
