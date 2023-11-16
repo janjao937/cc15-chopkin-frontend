4b9961851919a41ce6e2bd21320b914186634067
Champ <droped17@gmail.com>
2023-11-16 19:07:58 +0700

    merge devolop

--- src/contexts/ResAllContext.jsx
+++ src/contexts/ResAllContext.jsx
@@ -6,8 +6,7 @@ export const ResAllContext = createContext();
 export default function ResAllContextProvider({ children }) {
   // const { reviewId } = useParams();
   const [getRestaurantAll, setGetRestaurantAll] = useState([]);
-  const [scoreReview, setScoreReview] = useState([]);
-  const [scoreAvg, setScoreAvg] = useState([]);
+
 
   useEffect(() => {
     const fetchResAll = async () => {
@@ -22,34 +21,12 @@ export default function ResAllContextProvider({ children }) {
     fetchResAll();
   }, []);
 
-  const fetchScore = async (reviewId) => {
-    try {
-      const res = await axios.get(`/review/${reviewId}`);
-      console.log("Review ==>", res.data);
-      setScoreReview(res.data);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  const fetchScoreAvg = async () => {
-    try {
-      const res = await axios.get(`/restaurant/review-score`);
-      console.log("scoreAvg ==>", res.data);
-      setScoreAvg(res.data);
-    } catch (err) {
-      console.log(err);
-    }
-  };
+  
 
   return (
     <ResAllContext.Provider
       value={{
         getRestaurantAll,
-        fetchScore,
-        scoreReview,
-        fetchScoreAvg,
-        scoreAvg,
       }}
     >
       {children}
